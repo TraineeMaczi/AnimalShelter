99a55b9e22fc50cb4139c85636d2a969be27db43
xXElliotXx <karolina.makowska.kk@gmail.com>
2018-06-15 22:48:14 +0200

    Zadanie 1

--- AnimalShelter/src/animalshelter/Shelter.java
+++ AnimalShelter/src/animalshelter/Shelter.java
@@ -1,5 +1,23 @@
 package animalshelter;
-public class Shelter {
- 
+public class Shelter 
+{
+    private double AmountOfAllPlaces;
+    private double AmountOfFreePlaces; 
     
+    public double GetAmountOfAllPlaces()
+    {
+     return AmountOfAllPlaces;
+    }
+    public void SetAmountOfAllPlaces(double AmountOfAllPlaces )
+    {
+        this.AmountOfAllPlaces=AmountOfAllPlaces;
+    }
+    public double GetAmountOfFreePlaces()
+    {
+        return AmountOfFreePlaces;
+    }
+    private void SetAmountOfFreePlaces(double AmountOfFreePlaces )
+    {
+        this.AmountOfFreePlaces=AmountOfFreePlaces;
+    }
 }
